d5ef3f52349b74216b1d6777988724ccb03894bd
KateNegrienko <katya@negrienko.com>
2016-06-27 13:47:50 +0300

    refresh button add lint

--- src/refresh_button/refresh_button.js
+++ src/refresh_button/refresh_button.js
@@ -3,65 +3,66 @@
  * @copyright Digital Living Software Corp. 2014-2016
  */
 
-/* global angular */
-
-(function () {
+(function (angular) {
     'use strict';
 
-    var thisModule = angular.module("pipRefreshButton", ['ngMaterial']);
+    var thisModule = angular.module('pipRefreshButton', ['ngMaterial']);
 
-    thisModule.directive('pipRefreshButton',  
-        function($parse) {
+    thisModule.directive('pipRefreshButton',
+        function ($parse) {
             return {
                 restrict: 'EA',
                 scope: false,
-                template: String()
-                    + '<md-button class="pip-refresh-button" tabindex="-1" ng-click="onClick($event)" aria-label="REFRESH">'
-                    + '<md-icon md-svg-icon="icons:refresh"></md-icon>'
-                    + ' <span class="pip-refresh-text"></span>'
-                    + '</md-button>',
+                template: String() +
+                '<md-button class="pip-refresh-button" tabindex="-1" ng-click="onClick($event)" aria-label="REFRESH">' +
+                '<md-icon md-svg-icon="icons:refresh"></md-icon>' +
+                '<span class="pip-refresh-text"></span>' +
+                '</md-button>',
                 replace: false,
                 link: function ($scope, $element, $attrs) {
-                    var
+                    var width, text, show,
                         textGetter = $parse($attrs.pipText),
                         visibleGetter = $parse($attrs.pipVisible),
                         refreshGetter = $parse($attrs.pipRefresh),
                         $button = $element.children('.md-button'),
                         $text = $button.children('.pip-refresh-text');
 
-                    var show = function() {
+                    show = function () {
                         // Set a new text
-                        var text = textGetter($scope);
+                        text = textGetter($scope);
                         $text.text(text);
 
                         // Show button
                         $button.show();
-                        
+
                         // Adjust position
-                        var width = $button.width();
-                        $button.css('margin-left', '-' + (width / 2) + 'px');
+                        width = $button.width();
+                        $button.css('margin-left', '-' + width / 2 + 'px');
                     };
 
                     function hide() {
-                        $button.hide();  
+                        $button.hide();
+                    }
+
+                    $scope.onClick = function () {
+                        refreshGetter($scope);
                     };
 
-                    $scope.onClick = function(event) {
-                        refreshGetter($scope);  
-                    };
-
-                    $scope.$watch(visibleGetter, function(newValue) {
-                        if (newValue == true) show();
-                        else hide();
+                    $scope.$watch(visibleGetter, function (newValue) {
+                        if (newValue) {
+                            show();
+                        } else {
+                            hide();
+                        }
                     });
 
-                    $scope.$watch(textGetter, function(newValue) {
+                    $scope.$watch(textGetter, function (newValue) {
                         $text.text(newValue);
                     });
                 }
-            }
+            };
         }
     );
 
-})();
+})(window.angular);
 
