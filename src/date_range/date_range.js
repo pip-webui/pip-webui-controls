f981f78591a0cedf540a3f562d2e2fbf6feb9d77
KateNegrienko <katya@negrienko.com>
2016-06-27 11:28:19 +0300

    date range

--- src/date_range/date_range.js
+++ src/date_range/date_range.js
@@ -15,8 +15,7 @@
 
 (function () {
     'use strict';
-
-    var thisModule = angular.module("pipDateRange", []);
+    var thisModule = angular.module('pipDateRange', []);
 
     thisModule.directive('pipDateRange',
         function () {
@@ -34,17 +33,21 @@
                 },
                 templateUrl: 'date_range/date_range.html',
                 controller: 'pipDateRangeController'
-            }
+            };
         });
 
     thisModule.controller('pipDateRangeController',
         function ($scope, $element, pipTranslate, $mdMedia, $rootScope) {
-            var bufMonth,
-                currentDate = new Date(),
-                currentYear = currentDate.getUTCFullYear(),
-                currentMonth = currentDate.getUTCMonth() + 1,
-                currentDay = currentDate.getUTCDate(),
+            var currentDate,
+                currentYear,
+                currentMonth,
+                currentDay,
                 prevState = {}, currentState = {};
+
+            currentDate = new Date();
+            currentYear = currentDate.getUTCFullYear();
+            currentMonth = currentDate.getUTCMonth() + 1;
+            currentDay = currentDate.getUTCDate();
             $scope.daysWeek = {
                 en: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
                 ru: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
@@ -55,9 +58,11 @@
             };
 
             $scope.onMonthChanged = function () {
-                if ($scope.pipDateRangeType == 'weekly') {
-                    var date = new Date(Date.UTC($scope.year, $scope.month - 1, 1));
-                    var dayOfWeek = date.getUTCDay() ? date.getUTCDay() : 7;
+                if ($scope.pipDateRangeType === 'weekly') {
+                    var date, dayOfWeek;
+
+                    date = new Date(Date.UTC($scope.year, $scope.month - 1, 1));
+                    dayOfWeek = date.getUTCDay() ? date.getUTCDay() : 7;
                     $scope.week = getWeekByDate(dayOfWeek, $scope.month - 1, $scope.year);
                     correctWeek();
                     adjustWeek();
@@ -68,9 +73,11 @@
             };
 
             $scope.onYearChanged = function () {
-                if ($scope.pipDateRangeType == 'weekly') {
-                    var date = new Date(Date.UTC($scope.year, $scope.month - 1, 1));
-                    var dayOfWeek = date.getUTCDay() ? date.getUTCDay() : 7;
+                var date, dayOfWeek;
+
+                date = new Date(Date.UTC($scope.year, $scope.month - 1, 1));
+                dayOfWeek = date.getUTCDay() ? date.getUTCDay() : 7;
+                if ($scope.pipDateRangeType === 'weekly') {
                     $scope.week = getWeekByDate(dayOfWeek, $scope.month - 1, $scope.year);
                     adjustWeek();
                     correctWeek();
@@ -81,7 +88,7 @@
             };
 
             $scope.onWeekChange = function () {
-                if ($scope.pipDateRangeType == 'weekly') {
+                if ($scope.pipDateRangeType === 'weekly') {
                     adjustWeek();
                     correctWeek();
                 } else {
@@ -92,17 +99,17 @@
 
             // visibility
             $scope.isDay = function () {
-                return $scope.pipDateRangeType == 'daily';
+                return $scope.pipDateRangeType === 'daily';
             };
 
             $scope.isWeek = function () {
-                return $scope.pipDateRangeType == 'weekly';
+                return $scope.pipDateRangeType === 'weekly';
             };
 
             $scope.isMonth = function () {
-                return (($scope.pipDateRangeType == 'daily') ||
-                ($scope.pipDateRangeType == 'weekly') ||
-                ($scope.pipDateRangeType == 'monthly'));
+                return $scope.pipDateRangeType === 'daily' ||
+                    $scope.pipDateRangeType === 'weekly' ||
+                    $scope.pipDateRangeType === 'monthly';
             };
 
             $scope.onChange = function () {
@@ -127,24 +134,27 @@
                 $scope.shortMonths = monthList(true);
                 $scope.years = yearList();
 
-            };
+            }
 
             function correctWeek() {
-                if (!prevState.model || (prevState.model && prevState.model.getTime() >= $scope.model.getTime())) {
+                if (!prevState.model || prevState.model && prevState.model.getTime() >= $scope.model.getTime()) {
                     // if shift week -> increase month
                     if ($scope.weeks && $scope.weeks[$scope.week] && $scope.weeks[$scope.week].id <= 0) {
-                        if ($scope.month < 12) $scope.month += 1;
-                        else {
+                        if ($scope.month < 12) {
+                            $scope.month += 1;
+                        } else {
                             $scope.month = 1;
                             $scope.year += 1;
                         }
                         fillLists();
                     }
                 }
-            };
+            }
 
             function init() {
-                var value = $scope.model ? new Date($scope.model) : null;
+                var value;
+
+                value = $scope.model ? new Date($scope.model) : null;
                 $scope.day = value ? value.getUTCDate() : null;
                 $scope.month = value ? value.getUTCMonth() + 1 : null;
                 $scope.year = value ? value.getUTCFullYear() : null;
@@ -152,7 +162,7 @@
 
                 fillLists();
 
-                if ($scope.pipDateRangeType == 'weekly') {
+                if ($scope.pipDateRangeType === 'weekly') {
                     correctWeek();
                     adjustWeek();
                 } else {
@@ -170,7 +180,7 @@
 
             // React on changes
             $scope.$watch('model', function (newValue, oldValue) {
-                if (newValue != oldValue) {
+                if (newValue !== oldValue) {
                     getValue(newValue);
                 }
             });
@@ -180,60 +190,69 @@
             });
 
             $scope.$watch('pipDateRangeType', function (newValue, oldValue) {
-                if ((newValue != oldValue) && (oldValue)) {
+                if (newValue !== oldValue && oldValue) {
                     init();
                 }
             });
 
             $scope.onYearClick = function () {
-                if ($scope.year == null) $scope.year = currentYear;
+                if (!$scope.year) {
+                    $scope.year = currentYear;
+                }
             };
 
             $scope.onMonthClick = function () {
-                if ($scope.month == null) $scope.month = currentMonth;
+                if (!$scope.month) {
+                    $scope.month = currentMonth;
+                }
             };
 
             $scope.onDayClick = function () {
-                if ($scope.year == null) $scope.day = currentDay;
+                if (!$scope.year) {
+                    $scope.day = currentDay;
+                }
             };
-
-            return;
-
             // ---------------------------------------------------------------------------------------
 
             function getCountDay(month, year) {
                 var count = 31;
 
-                if (month == 4 || month == 6 || month == 9 || month == 11) {
+                if (month === 4 || month === 6 || month === 9 || month === 11) {
                     count = 30;
-                } else if (month == 2) {
-                    if (year)
-                    // Calculate leap year (primitive)
-                        count = year % 4 == 0 ? 29 : 28;
-                    else count = 28;
+                } else if (month === 2) {
+                    if (year) {
+                        // Calculate leap year (primitive)
+                        count = year % 4 === 0 ? 29 : 28;
+                    } else {
+                        count = 28;
+                    }
                 }
+
                 return count;
             }
 
             function dayList(month, year) {
-                var count = getCountDay(month, year);
+                var count, days, i, ln;
 
+                ln = $rootScope.$language || 'en';
+                count = getCountDay(month, year);
                 $scope.nameDays = [];
-                var days = [];
-                for (var i = 1; i <= count; i++) {
+                days = [];
+                for (i = 1; i <= count; i++) {
                     days.push(i);
-
-                    $scope.nameDays.push($scope.daysWeek[$rootScope.$language || 'en'][new Date(year, month - 1, i).getDay()]);
+                    $scope.nameDays.push($scope.daysWeek[ln][new Date(year, month - 1, i).getDay()]);
                 }
+
                 return days;
             }
 
             function getWeekByDate(day, month, year) {
-                var date = new Date(Date.UTC(year, month, day));
-                var dayOfWeek = date.getUTCDay() ? date.getUTCDay() : 7;
-                var startWeek;
+                var date, dayOfWeek, startWeek;
 
-                if (dayOfWeek == 1) {
+                date = new Date(Date.UTC(year, month, day));
+                dayOfWeek = date.getUTCDay() ? date.getUTCDay() : 7;
+
+                if (dayOfWeek === 1) {
                     startWeek = day;
                 } else {
                     startWeek = day + 1 - dayOfWeek;
@@ -243,16 +262,20 @@
             }
 
             function getWeek(day, countDay, countPrevMonthDay) {
-                var endDay = (day + 6) > countDay ? countDay - day - 6 : day + 6;
-                var startDay = (day > 0) ? day : countPrevMonthDay + day;
-                return (startDay).toString() + ' - ' + (Math.abs(endDay)).toString();
+                var endDay, startDay;
+
+                endDay = day + 6 > countDay ? countDay - day - 6 : day + 6;
+                startDay = day > 0 ? day : countPrevMonthDay + day;
+
+                return startDay.toString() + ' - ' + (Math.abs(endDay)).toString();
             }
 
             function weekList(month, year) { // возвращает список начала надели
-                var weeks = [];  // в массиве первые дни недели в текущем месяце
-                var countDay = getCountDay(month, year);
-                var countPrevMonthDay;
-                var startWeek = getWeekByDate(1, month - 1, year);
+                var weeks, countDay, countPrevMonthDay, startWeek;
+
+                weeks = [];  // в массиве первые дни недели в текущем месяце
+                countDay = getCountDay(month, year);
+                startWeek = getWeekByDate(1, month - 1, year);
 
                 countPrevMonthDay = month - 1 ? getCountDay(month - 1, year) : getCountDay(12, year - 1);
                 while (startWeek < countDay + 1) {
@@ -267,21 +290,21 @@
             }
 
             function monthList(isShort) {
-                var months = [];
+                var months, i;
 
-                for (var i = 1; i <= 12; i++) {
+                months = [];
+                for (i = 1; i <= 12; i++) {
                     months.push({
                         id: i,
                         name: pipTranslate.translate('MONTH_' + i)
-                    })
+                    });
                 }
 
                 return months;
             }
 
             function yearList() {
-                var
-                    startYear,
+                var startYear, i,
                     endYear,
                     years = [];
 
@@ -303,15 +326,16 @@
                         endYear = currentYear + 50;
                         break;
                 }
-                if ($scope.timeMode == 'future') {
-                    for (var i = startYear; i <= endYear; i++) {
+                if ($scope.timeMode === 'future') {
+                    for (i = startYear; i <= endYear; i++) {
                         years.push(i);
                     }
                 } else {
-                    for (var i = endYear; i >= startYear; i--) {
+                    for (i = endYear; i >= startYear; i--) {
                         years.push(i);
                     }
                 }
+
                 return years;
             }
 
@@ -327,7 +351,7 @@
                         $scope.day = 1;
                         break;
                     default:
-                        if ($scope.days.length != days.length) {
+                        if ($scope.days.length !== days.length) {
                             if ($scope.day > days.length) {
                                 $scope.day = days.length;
                             }
@@ -338,22 +362,26 @@
             }
 
             function adjustWeek() {
-                var weeks = weekList($scope.month, $scope.year);
+                var weeks;
+
+                weeks = weekList($scope.month, $scope.year);
                 $scope.week = getWeekByDate($scope.week, $scope.month - 1, $scope.year);
                 $scope.weeks = weeks;
             }
 
-            function getValue(value) {
-                value = value ? new Date(value) : null;
+            function getValue(v) {
+                var value, day, month, year, week;
+
+                value = v ? new Date(v) : null;
                 // Define values
-                var day = value ? value.getUTCDate() : null;
-                var month = value ? value.getUTCMonth() + 1 : null;
-                var year = value ? value.getUTCFullYear() : null;
-                var week = value ? getWeekByDate(day, month - 1, year) : null;
+                day = value ? value.getUTCDate() : null;
+                month = value ? value.getUTCMonth() + 1 : null;
+                year = value ? value.getUTCFullYear() : null;
+                week = value ? getWeekByDate(day, month - 1, year) : null;
                 // Exit if nothing to update
-                if ($scope.day == day && $scope.month == month
-                    && $scope.year == year && $scope.week == week)
+                if ($scope.day === day && $scope.month === month && $scope.year === year && $scope.week === week) {
                     return;
+                }
                 // Assign values to scope
                 $scope.day = day;
                 $scope.month = month;
@@ -367,7 +395,8 @@
 
             function setValue() {
                 var value;
-                if ($scope.pipDateRangeType == 'weekly') {
+
+                if ($scope.pipDateRangeType === 'weekly') {
                     value = new Date($scope.year, $scope.month - 1, $scope.week, 0, 0, 0, 0);
                     value = new Date(value.getTime() - value.getTimezoneOffset() * 60000);
                     $scope.model = value;
