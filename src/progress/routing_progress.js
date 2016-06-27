98d74d73802efefd41e5434bd1714663dd1cc586
KateNegrienko <katya@negrienko.com>
2016-06-27 13:43:14 +0300

    routing progress add lint

--- src/progress/routing_progress.js
+++ src/progress/routing_progress.js
@@ -1,24 +1,22 @@
 /**
  * @file Routing progress control
- * @description 
- * This progress control is enabled by ui router 
+ * @description
+ * This progress control is enabled by ui router
  * while switching between pages
  * @copyright Digital Living Software Corp. 2014-2016
  */
 
-/* global _, angular */
-
-(function () {
+(function (angular) {
     'use strict';
 
-    var thisModule = angular.module("pipRoutingProgress", ['ngMaterial']);
+    var thisModule = angular.module('pipRoutingProgress', ['ngMaterial']);
 
-    thisModule.directive('pipRoutingProgress', function() {
-       return {
-           restrict: 'EA',
-           replace: true,
-           templateUrl: 'progress/routing_progress.html'
-       };
+    thisModule.directive('pipRoutingProgress', function () {
+        return {
+            restrict: 'EA',
+            replace: true,
+            templateUrl: 'progress/routing_progress.html'
+        };
     });
-   
-})();
+
+})(window.angular);
