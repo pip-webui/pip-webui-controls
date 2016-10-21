1c4e43122a177ae6517ccf21ceabc6c44cc58841
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-20 19:42:33 -0500

    Changed methods in nav services

--- dist/pip-webui-controls.js
+++ dist/pip-webui-controls.js
@@ -1,3 +1,28 @@
+/**
+ * @file Registration of basic WebUI controls
+ * @copyright Digital Living Software Corp. 2014-2016
+ */
+
+/* global angular */
+
+(function (angular) {
+    'use strict';
+
+    angular.module('pipControls', [
+        'pipMarkdown',
+        'pipColorPicker',
+        'pipRoutingProgress',
+        'pipPopover',
+        'pipImageSlider',
+        'pipToasts',
+        'pipUnsavedChanges',
+        'pipControls.Translate',
+        'pipDraggable'
+    ]);
+
+})(window.angular);
+
+
 (function(module) {
 try {
   module = angular.module('pipControls.Templates');
@@ -91,31 +116,6 @@ module.run(['$templateCache', function($templateCache) {
     '</md-toast>');
 }]);
 })();
-
-/**
- * @file Registration of basic WebUI controls
- * @copyright Digital Living Software Corp. 2014-2016
- */
-
-/* global angular */
-
-(function (angular) {
-    'use strict';
-
-    angular.module('pipControls', [
-        'pipMarkdown',
-        'pipColorPicker',
-        'pipRoutingProgress',
-        'pipPopover',
-        'pipImageSlider',
-        'pipToasts',
-        'pipUnsavedChanges',
-        'pipControls.Translate',
-        'pipDraggable'
-    ]);
-
-})(window.angular);
-
 
 /**
  * @file Color picker control
