e6f28044082d4042a6a2cecf0e685108299bb708
KateNegrienko <katya@negrienko.com>
2016-10-27 17:09:52 +0300

    Jumbotron

--- samples/jumbotron/jumbotron.js
+++ samples/jumbotron/jumbotron.js
@@ -9,6 +9,19 @@
         function($scope, $mdMedia) {
             //....
             $scope.$mdMedia =  $mdMedia;
+            $scope.items=[
+                {
+                    text1:'7a26e1804422',
+                    text2:'804422',
+                    text3:'3',
+                    text4:'12 min 34 sec ago'
+                }, {
+                    text1:'7878a26e1804422',
+                    text2:'POReceipt#: 804422',
+                    text3:'4',
+                    text4:'12 min 34 sec ago'
+                }
+            ];
         }
     );
 
