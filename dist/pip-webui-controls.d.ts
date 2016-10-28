ed04a8a5bb9e4bdd972348726ef892970740e97a
KateNegrienko <katya@negrienko.com>
2016-10-28 09:52:48 +0300

    jumbotron update

--- dist/pip-webui-controls.d.ts
+++ dist/pip-webui-controls.d.ts
@@ -1,24 +1,24 @@
-/// <reference path="../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
+
+
+
+
+
+
+
+
 declare var marked: any;
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="../../typings/tsd.d.ts" />
+
+
+
+
