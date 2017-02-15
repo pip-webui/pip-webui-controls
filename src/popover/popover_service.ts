c1e192851232972397e2c5a9ab83c2b32bfbd81f
KateNegrienko <katya@negrienko.com>
2017-02-15 16:06:59 +0200

    Delete old code in popover service

--- src/popover/popover_service.ts
+++ src/popover/popover_service.ts
@@ -1,6 +1,5 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-
 export class PopoverService {
   
     private _$timeout;
@@ -55,55 +54,3 @@ export class PopoverService {
         .module('pipPopover.Service', [])
         .service('pipPopoverService', PopoverService);
 })();
-
-/*
-(function () {
-    'use strict';
-
-    var thisModule = angular.module('pipPopover.Service', []);
-
-    thisModule.service('pipPopoverService',
-        function ($compile, $rootScope, $timeout) {
-            var popoverTemplate;
-
-            popoverTemplate = "<div class='pip-popover-backdrop {{ params.class }}' ng-controller='params.controller'" +
-                " tabindex='1'> <pip-popover pip-params='params'> </pip-popover> </div>";
-
-            return {
-                show: onShow,
-                hide: onHide,
-                resize: onResize
-            };
-
-            function onShow(p) {
-                var element, scope, params, content;
-
-                element = $('body');
-                if (element.find('md-backdrop').length > 0) { return; }
-                onHide();
-                scope = $rootScope.$new();
-                params = p && _.isObject(p) ? p : {};
-                scope.params = params;
-                scope.locals = params.locals;
-                content = $compile(popoverTemplate)(scope);
-                element.append(content);
-            }
-
-            function onHide() {
-                var backdropElement = $('.pip-popover-backdrop');
-
-                backdropElement.removeClass('opened');
-                $timeout(function () {
-                    backdropElement.remove();
-                }, 100);
-            }
-
-            function onResize() {
-                $rootScope.$broadcast('pipPopoverResize');
-            }
-
-        }
-    );
-
-})();
-*/
