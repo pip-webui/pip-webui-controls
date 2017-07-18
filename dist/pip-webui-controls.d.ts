2efe37632ff4ded7ddf9dfa009451cbb8fa633b2
Anastas Fonotov <anastasf@gmail.com>
2017-07-18 16:20:01 +0300

    'rebuild'

--- dist/pip-webui-controls.d.ts
+++ dist/pip-webui-controls.d.ts
@@ -19,6 +19,15 @@ export interface IImageSliderService {
 var marked: any;
 
 
+export interface IPopoverService {
+    show(p: Object): void;
+    hide(): void;
+    resize(): void;
+}
+
+
+
+
 
 export interface IToastService {
     showNextToast(): void;
@@ -45,15 +54,6 @@ export class Toast {
     successCallback: Function;
     cancelCallback: Function;
 }
-
-
-
-export interface IPopoverService {
-    show(p: Object): void;
-    hide(): void;
-    resize(): void;
-}
-
 
 
 }
