b48a40449025713049a3950923d38e6c6cd37cc0
Anastas Fonotov <anastasf@gmail.com>
2017-07-28 13:08:24 +0300

    'rebuild'

--- dist/pip-webui-controls.d.ts
+++ dist/pip-webui-controls.d.ts
@@ -2,6 +2,18 @@ declare module pip.controls {
 
 
 
+var marked: any;
+
+
+export interface IPopoverService {
+    show(p: Object): void;
+    hide(): void;
+    resize(): void;
+}
+
+
+
+
 export interface IImageSliderService {
     registerSlider(sliderId: string, sliderScope: any): void;
     removeSlider(sliderId: string): void;
@@ -14,9 +26,6 @@ export interface IImageSliderService {
 
 
 
-
-
-var marked: any;
 
 
 
@@ -45,15 +54,6 @@ export class Toast {
     successCallback: Function;
     cancelCallback: Function;
 }
-
-
-
-export interface IPopoverService {
-    show(p: Object): void;
-    hide(): void;
-    resize(): void;
-}
-
 
 
 }
